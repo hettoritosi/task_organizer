02031dd4accc7614a264ff87f3e83e5e36c821f0
hettoritosi <hettori@gmail.com>
2022-04-07 17:21:53 -0300

    Testing3

--- config/application.rb
+++ config/application.rb
@@ -2,9 +2,6 @@ require_relative "boot"
 
 require "rails/all"
 
-config.autoloader = :classic
-
-
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
