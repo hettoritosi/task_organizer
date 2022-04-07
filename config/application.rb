f2904f71d1708aec95d2da197876a34683cc7fbb
hettoritosi <hettori@gmail.com>
2022-04-07 16:53:20 -0300

    Heroku problem

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,7 @@ require_relative "boot"
 
 require "rails/all"
 
+config.autoload = :classic
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
